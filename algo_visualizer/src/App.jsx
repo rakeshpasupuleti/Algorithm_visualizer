6f0af3f8ef783ff3e88e8fadb45ffe07d13a531b
rakeshpasupuleti <rakeshpasupuleti550@gmail.com>
2024-08-06 11:24:04 -0400

    Added the code for the Algo Visualizer

---
+++ algo_visualizer/src/App.jsx
@@ -0,0 +1,18 @@
+import './App.css'
+import Grid from './components/grid/grid'
+import Navbar from './components/navbar/navbar'
+
+
+
+function App() {
+
+
+  return (
+    <div className="App">
+     <Navbar></Navbar>
+     <Grid></Grid>
+    </div>
+  )
+}
+
+export default App
