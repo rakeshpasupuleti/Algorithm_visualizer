df4cc06cea064ddd5765c9bc882018e1bf70a664
rakeshpasupuleti <rakeshpasupuleti550@gmail.com>
2024-08-06 11:56:21 -0400

    Modified vite confic

--- algo_visualizer/vite.config.js
+++ algo_visualizer/vite.config.js
@@ -1,7 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  base: '/rakeshpasupuleti/', // Replace <your-repo-name> with your repository name
+  plugins: [react()]
 })
